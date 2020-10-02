eedab8149050e3f4273820b2de704b8ea43d9db5
rishabh0211 <rishabh0211@gmail.com>
2020-10-03 02:54:55 +0530

    tray icon and code structure

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron');
-const log = require('electron-log');
+const path = require("path");
+const { app, BrowserWindow, Menu, ipcMain, Tray } = require("electron");
+const log = require("electron-log");
+const MainWindow = require("./MainWindow");
+const AppTray = require("./AppTray");
 const Store = require("./Store");
 // Set env
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = "development";
 
-const isDev = process.env.NODE_ENV !== 'production' ? true : false;
-const isMac = process.platform === 'darwin' ? true : false;
+const isDev = process.env.NODE_ENV !== "production" ? true : false;
+const isMac = process.platform === "darwin" ? true : false;
 
 let mainWindow;
+let tray;
 
 // Init store and defaults
 const store = new Store({
@@ -21,25 +25,10 @@ const store = new Store({
 });
 
 function createMainWindow() {
-  mainWindow = new BrowserWindow({
-    title: 'CPUInfo',
-    width: isDev ? 800 : 355,
-    height: 500,
-    icon: './assets/icons/icon.png',
-    resizable: isDev ? true : false,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-
-  if (isDev) {
-    mainWindow.webContents.openDevTools();
-  }
-
-  mainWindow.loadFile('./app/index.html');
+  mainWindow = new MainWindow("./app/index.html", isDev);
 }
 
-app.on('ready', () => {
+app.on("ready", () => {
   createMainWindow();
   mainWindow.webContents.on("dom-ready", () => {
     mainWindow.webContents.send("settings:get", store.get("settings"));
@@ -47,22 +36,45 @@ app.on('ready', () => {
 
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on("close", e => {
+    if (!app.isQuitting) {
+      e.preventDefault();
+      mainWindow.hide();
+    }
+    return true;
+  });
+
+  const icon = path.join(__dirname, "assets", "icons", "tray_icon.png");
+  // Create tray
+  tray = new AppTray(icon, mainWindow);
+
+  // mainWindow.on("close", () => mainWindow = null);
 });
 
 const menu = [
-  ...(isMac ? [{ role: 'appMenu' }] : []),
+  ...(isMac ? [{ role: "appMenu" }] : []),
   {
-    role: 'fileMenu',
+    role: "fileMenu",
+  },
+  {
+    label: "View",
+    submenu: [
+      {
+        label: "Toggle Navigation",
+        click: () => mainWindow.webContents.send("nav:toggle")
+      }
+    ]
   },
   ...(isDev
     ? [
       {
-        label: 'Developer',
+        label: "Developer",
         submenu: [
-          { role: 'reload' },
-          { role: 'forcereload' },
-          { type: 'separator' },
-          { role: 'toggledevtools' },
+          { role: "reload" },
+          { role: "forcereload" },
+          { type: "separator" },
+          { role: "toggledevtools" },
         ],
       },
     ]
@@ -75,13 +87,13 @@ ipcMain.on("settings:set", (e, value) => {
   mainWindow.webContents.send("settings:get", store.get("settings"));
 });
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   if (!isMac) {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createMainWindow();
   }
